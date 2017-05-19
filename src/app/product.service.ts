caf64fe48e46dd8e3355c767f7904bd8184793c9
sukhbirsaini <sukhbirsaini04@gmail.com>
2017-05-20 03:16:20 +0530

    mongo

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,6 +14,7 @@ import { IProduct } from 'app/iproduct';
 @Injectable()
 export class ProductService {
     private _productUrl = 'https://inventorymanagementapp.herokuapp.com/api/';
+    
 
     constructor(private _http: Http, private localStorageService: LocalStorageService) { }
 
