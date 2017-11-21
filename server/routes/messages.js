076ed682fbb7593cb48ba93391141bf1fbc5eacf
sukhbirsaini <sukhbir.saini@nagarro.com>
2017-11-21 14:35:43 +0530

    c

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -46,6 +46,96 @@ bot.dialog('Greeting', [
 ]).triggerAction({
   matches: 'Greeting'
 });
+var seeAllBuses = "See All Buses";
+var FindBy = "Find Bus";
+var CardNames = [seeAllBuses, FindBy];
+bot.dialog('Help', [
+  function (session, args, next) {
+    builder.Prompts.choice(session, 'I can help you with the following please select from below? Please enter 1 or 2!', CardNames);
+  },
+  function (session, results) {
+    var selectedOption = results.response.entity;
+    if (selectedOption == CardNames[1]) {
+      searchBy(session);
+    } else if (selectedOption == CardNames[0]) {
+      session.conversationData.filter = undefined;
+      showingProducts(session);
+    }
+  }
+]).triggerAction({
+  matches: 'Help'
+});
+var Name = 'Search by Name of bus';
+var Origin = 'Search by Origin of bus';
+var Destination = 'Search by Destination of bus';
+var CategoriesOfHelp = [Name, Origin, Destination];
+var searchBy = function (session) {
+  var msg = new builder.Message(session)
+    .textFormat(builder.TextFormat.xml)
+    .attachmentLayout(builder.AttachmentLayout.carousel)
+    .attachments([
+      new builder.HeroCard(session)
+        .title("Search Buses")
+        .text("You can search the buses with the following:")
+        .buttons([
+          builder.CardAction.imBack(session, CategoriesOfHelp[0], "Name of bus"),
+          builder.CardAction.imBack(session, CategoriesOfHelp[1], "Origin of bus"),
+          builder.CardAction.imBack(session, CategoriesOfHelp[2], "Destination of bus"),
+        ])
+    ]);
+  session.send(msg);
+};
+
+bot.dialog('Find', [
+  function (session, args, next) {
+    session.dialogData.CategoriesFound = {
+      // CategoryEntity_Name: builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Name') ? builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Name').type : '',
+      // CategoryEntity_Type: builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Type') ? builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Type').type : '',
+      CategoryEntity_Type: builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city') ? builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city').type : '',
+      // CategoryEntity_Discount: builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Discount').type,
+      // CategoryEntity_Price: builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Price').type,
+      // CategoryEntity_Available: builder.EntityRecognizer.findEntity(args.intent.entities, 'Categories::Available').type,
+    };
+    session.conversationData.filter = {
+      name: session.conversationData.filter ? (session.conversationData.filter.name ? session.conversationData.filter.name : null) : null,
+      type: session.conversationData.type ? session.conversationData.filter.type : null,
+      // Discount: null,
+      // price: null,
+      // availability: null
+    };
+
+    if (session.dialogData.CategoriesFound) {
+      if (session.dialogData.CategoriesFound.CategoryEntity_Name && !session.dialogData.CategoriesFound.CategoryEntity_Type) {
+        builder.Prompts.text(session, 'Please enter the name of the product you want to search?');
+        next();
+      } else if (session.dialogData.CategoriesFound.CategoryEntity_Type && !session.dialogData.CategoriesFound.CategoryEntity_Name) {
+        builder.Prompts.text(session, 'Please enter the type of the product you want to search?');
+        next();
+      } else if (session.dialogData.CategoriesFound.CategoryEntity_Type && session.dialogData.CategoriesFound.CategoryEntity_Name) {
+        builder.Prompts.text(session, 'Please enter the name and type of the product you want to search? e.g Name-Type');
+        next();
+      }
+    }
+  },
+  function (session, results, next) {
+    if (session.dialogData.CategoriesFound.CategoryEntity_Name && !session.dialogData.CategoriesFound.CategoryEntity_Type) {
+      var name = results.response;
+      session.conversationData.filter.name = name ? name : '';
+    } else if (session.dialogData.CategoriesFound.CategoryEntity_Type && !session.dialogData.CategoriesFound.CategoryEntity_Name) {
+      var type = results.response;
+      session.conversationData.filter.type = type ? type : '';
+    } else if (session.dialogData.CategoriesFound.CategoryEntity_Type && session.dialogData.CategoriesFound.CategoryEntity_Name) {
+      var splitString = results.response.replace(/ /g, '').split('-');
+      session.conversationData.filter = {
+        name: splitString[0],
+        type: splitString[1]
+      }
+    }
+    showingProducts(session);
+  }
+]).triggerAction({
+  matches: 'Search'
+})
 
 bot.dialog('/', function (session, args) {
   session.send("Sorry couldn't understant what you just typed. Please enter 'Help'");
