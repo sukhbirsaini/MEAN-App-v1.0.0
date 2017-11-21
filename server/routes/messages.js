2b588c377c4249bc7a869501ff6c6f0eaf1e9ba5
sukhbirsaini <sukhbir.saini@nagarro.com>
2017-11-21 14:13:30 +0530

    commit

--- server/routes/messages.js
+++ server/routes/messages.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
 var _busBookingDetails = "../jsonData/busBooking.json"
+var request = require('request');
+
 //Include the library botbuilder
 let builder = require('botbuilder')
-console.log(_busBookingDetails);
+// console.log(_busBookingDetails);
+// request(_busBookingDetails, function (error, response, body) {
+//   if (error) {
+//     console.log('error:', error); // Print the error if one occurred
+//   } else {
+//     productsFiltered = JSON.parse(body);
+//     console.log(productsFiltered);
+//   }
+// });
 // Create chat connector with the default id and password
 let connector = new builder.ChatConnector({
-  appId: '07ba745f-24ec-4e2b-b841-d898016526ea',
-  appPassword: 'vnwvcfPVJA69=]hCBY362!%'
+  appId: "07ba745f-24ec-4e2b-b841-d898016526ea",
+  appPassword: "vnwvcfPVJA69=]hCBY362!%"
 })
 
-var bot = new builder.UniversalBot(connector, function (session) {
-  session.send("Sorry couldn't understant what you just typed. Please enter 'Help'");
-});
-var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9f1d74f2-f379-4f0a-b41d-5a5e460c1c29?subscription-key=84ac1bb781c64202b63e755f00314733&verbose=true&timezoneOffset=0&q=');
+var bot = new builder.UniversalBot(connector);
+var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/9f1d74f2-f379-4f0a-b41d-5a5e460c1c29?subscription-key=84ac1bb781c64202b63e755f00314733&spellCheck=true&verbose=true&timezoneOffset=0&q=');
 bot.recognizer(recognizer);
 
 bot.dialog('/', function (session, args) {
@@ -41,24 +49,6 @@ bot.dialog('Greetings', [
 ]).triggerAction({
   matches: 'Greetings'
 });
-
-// bot.dialog('Help', [
-//   function (session, args, next) {
-//     builder.Prompts.choice(session, 'I can help you with the following please select from below? Please enter 1 or 2!', CardNames);
-//   },
-//   function (session, results) {
-//     var selectedOption = results.response.entity;
-//     if (selectedOption == CardNames[1]) {
-//       searchBy(session);
-//     } else if (selectedOption == CardNames[0]) {
-//       session.conversationData.filter = undefined;
-//       showingProducts(session);
-//     }
-//   }
-// ]).triggerAction({
-//   matches: 'Help'
-// });
-
 
 
 router.post('/', connector.listen());
